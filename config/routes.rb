ac13bf434a925a21693b3f14d04d89d3c53c68c2
fiorella <fiorerut8@hotmail.com>
2022-12-14 19:52:19 -0300

    creacion de 3 vistas nuevas politica de cancelacion help y condiciones

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/cancel_policy', to: 'pages#cancel_policy'
+  get '/condiciones', to: 'pages#condiciones'
+  get '/help', to: 'pages#help'
   # Defines the root path route ("/")
   # root "articles#index"
   get '/misautos', to: 'cars#misautos'
